da9e9e439a89aca7e9f57bf3d46e194b338c3915
Alex Rosegrid <rosegrid58@gmail.com>
2026-06-01 13:16:20 +0530

    chore: initial "todo list" project setup

---
+++ src/todo_list/webpack.common.js
@@ -0,0 +1,31 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+    entry: "./src/index.js",
+    output: {
+        filename: "index.js",
+        path: resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
+            },
+        ]
+    },
+};
