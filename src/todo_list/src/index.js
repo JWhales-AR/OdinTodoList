be46181cf25df0bf92ea2cab66d699779964efb0
Alex Rosegrid <rosegrid58@gmail.com>
2026-06-07 23:07:02 +0530

    feat: design a dialog box for task updation

--- src/todo_list/src/index.js
+++ src/todo_list/src/index.js
@@ -1,6 +1,12 @@
 import "./style.css";
-import { mainElement, sideBar } from "./ui";
+import { mainElement, sideBar, makeTaskUpdateDialog } from "./ui";
 
 let body = document.querySelector("body");
+
+let taskUpdateDialogDummy = makeTaskUpdateDialog("?", "create");
+
 body.appendChild(sideBar);
 body.appendChild(mainElement);
+body.appendChild(taskUpdateDialogDummy);
+
+taskUpdateDialogDummy.showModal();
